9b7ea776cd346b1d4af3397edf9ee3bb66adf055
Haresh Chauhan <hareshc1999@gmail.com>
2025-10-23 16:26:07 +0530

    up

--- resources/views/sabpaisa/test.blade.php
+++ resources/views/sabpaisa/test.blade.php
@@ -9,7 +9,7 @@
 <body>
     <form action="{{ url('/sabpaisa/sandbox/request') }}" method="post">
         <input type="text" name="client_id" value="edutrade">
-        <input type="text" name="client_secret" value="c73ba053-bf3f-4c9e-88ae-9e49fd4534e4">
+        <input type="text" name="client_secret" value="1890b383-345a-42d3-88c7-6e80efd08460">
         <input type="text" name="payer_name" value="Haresh">
         <input type="text" name="payer_email" value="hareshc1999@gmail.com">
         <input type="text" name="payer_mobile" value="9106029220">
@@ -22,7 +22,7 @@
 
     <form action="{{ url('/sabpaisa/request') }}" method="post">
         <input type="text" name="client_id" value="edutrade">
-        <input type="text" name="client_secret" value="1890b383-345a-42d3-88c7-6e80efd08460">
+        <input type="text" name="client_secret" value="c73ba053-bf3f-4c9e-88ae-9e49fd4534e4">
         <input type="text" name="payer_name" value="Haresh">
         <input type="text" name="payer_email" value="hareshc1999@gmail.com">
         <input type="text" name="payer_mobile" value="9106029220">
