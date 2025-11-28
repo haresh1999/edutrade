be98cc64a8da944ea54ac48929b670ef739e2917
haresh <hareshc1999@gmail.com>
2025-11-28 10:35:32 +0000

    up

--- app/Http/Controllers/PayoutController.php
+++ app/Http/Controllers/PayoutController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-
 class PayoutController extends Controller
 {
     public function request()
     {
-        $url = "https://softtechapi.in/api/Payout/v3/Payout";
+        $curl = curl_init();
 
         $payload = [
             'mode' => 'IMPS',
@@ -27,29 +24,27 @@ class PayoutController extends Controller
             'otp' => rand(111111, 999999)
         ];
 
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            "Content-Type: application/json",
-            "x-qro-userid: 9656132740",
-            "x-qro-apikey: 1CBAE4927FC2438C80B689170266442A"
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://softtechapi.in/api/Payout/v3/Payout',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($payload),
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/json',
+                'x-qro-userid: 9656132740',
+                'x-qro-apikey: 1CBAE4927FC2438C80B689170266442A'
+            ],
         ]);
 
-        $response = curl_exec($ch);
-        $err = curl_error($ch);
+        $response = curl_exec($curl);
 
-        curl_close($ch);
+        curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #: " . $err;
-        } else {
-            echo $response;
-        }
-
-        return response()->json(['message' => 'Payout request received']);
+        return response()->json(json_decode($response));
     }
 }
